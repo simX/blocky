a8fa4ad906707a38f8ffdf1b2b80fccee1132e30
Simone Manganelli <simX@mac.com>
2016-05-28 12:48:45 -0700

    first working extension

---
+++ Blocky Extension/ActionRequestHandler.swift
@@ -0,0 +1,23 @@
+//
+//  ActionRequestHandler.swift
+//  Blocky
+//
+//  Created by Simone Manganelli on 28/05/16.
+//  Copyright © 2016 Simone Manganelli. All rights reserved.
+//
+
+import UIKit
+import MobileCoreServices
+
+class ActionRequestHandler: NSObject, NSExtensionRequestHandling {
+
+    func beginRequestWithExtensionContext(context: NSExtensionContext) {
+        let attachment = NSItemProvider(contentsOfURL: NSBundle.mainBundle().URLForResource("blockerList", withExtension: "json"))!
+    
+        let item = NSExtensionItem()
+        item.attachments = [attachment]
+    
+        context.completeRequestReturningItems([item], completionHandler: nil);
+    }
+    
+}
---
+++ Blocky/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Blocky
+//
+//  Created by Simone Manganelli on 28/05/16.
+//  Copyright © 2016 Simone Manganelli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
